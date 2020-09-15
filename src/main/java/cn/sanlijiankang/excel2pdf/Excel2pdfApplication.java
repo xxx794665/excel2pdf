e08c5941886347dfe1d1e3509ba8bd958c744582
xxx794665 <249939260@qq.com>
2020-09-15 10:51:32 +0800

    add application.yml

---
+++ src/main/java/cn/sanlijiankang/excel2pdf/Excel2pdfApplication.java
@@ -0,0 +1,13 @@
+package cn.sanlijiankang.excel2pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excel2pdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Excel2pdfApplication.class, args);
+	}
+
+}
